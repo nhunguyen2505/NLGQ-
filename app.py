67a561ba0ca9eee14113b49e35977461ccdc13cf
nhunguyen2505 <54281727+nhunguyen2505@users.noreply.github.com>
2023-10-16 15:57:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -235,8 +235,6 @@ def generate():
     return generated_text
         
 if __name__ == '__main__':
-    #  Debug/Development
-    app.run(debug=True, host="0.0.0.0", port="5000")
-    # Production
-    # http_server = WSGIServer(('', 5000), app)
-    # http_server.serve_forever()
+    from gevent.pywsgi import WSGIServer
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
